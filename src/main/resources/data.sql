aa26ee6bd28eacb15bb718b91c6960e40d8e001a
张震波10058115 <10058115@zte.intra>
2016-09-19 19:08:18 +0800

    comit src

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+insert into book1 (id, douban_id, title, url, description, owner_id,status,onboard_date) values(1,'25984046', 'Big Data日知录', 'http://book.douban.com/subject/25984046/','', 1,'idle','2015-01-01');
+insert into book1 (id, douban_id, title, url, description, owner_id,status,onboard_date) values(2,'25900156', 'Redis设计与实现', 'http://book.douban.com/subject/25900156/','', 1,'idle','2015-01-02');
+insert into book1 (id, douban_id, title, url, description, owner_id,status,onboard_date) values(3,'25741352', 'DSL实战', 'http://book.douban.com/subject/25741352/','', 2,'idle','2015-01-03');
+insert into book1 (id, douban_id, title, url, description, owner_id,status,onboard_date) values(4,'50368904', 'Think in java', 'http://book.douban.com/subject/50368904/','', 3,'idle','2015-01-05');
+
+insert into account (id,email,name,hash_password) values(1,'calvin.xiao@springside.io','Calvin','+2MunThvGcEfdYIFlT4NQQHt6z4=');
+insert into account (id,email,name,hash_password) values(2,'david.wang@springside.io','David','+2MunThvGcEfdYIFlT4NQQHt6z4=');
+insert into account (id,email,name,hash_password) values(3,'zzb205@163.com','Bob','QPfAH0GJUQAxrczZxgShKK2vmwA=');
+insert into account (id,email,name,hash_password) values(4,'ma.fei@163.com','Yema','QPfAH0GJUQAxrczZxgShKK2vmwA=');
+
+
