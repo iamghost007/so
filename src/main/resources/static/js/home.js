60c57541b0fbde9147edb2cea38867fb9859e525
Bob Zhang <zzb205@163.com>
2016-10-10 20:21:58 +0800

    add ajax ability

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,77 +1,119 @@
 $(function() {
-	$("#greeting").html($("#greeting").html() + "&nbsp;&nbsp;" + $.getGreetingTime()) ;
+	$("#greeting").html(
+			$("#greeting").html() + "&nbsp;&nbsp;" + $.getGreetingTime());
 
 	$.loadFunction("/products");
 
-    $("#tabs a").click(function () {  
-        $(this).tab('show');  
-    });  
-    
-    //ajax submit::http://www.cnblogs.com/nangong/archive/2013/07/23/3208302.html
+	$("#tabs a").click(function() {
+		$(this).tab('show');
+	});
 
 });
 
-jQuery.extend({
-	loadFunction:function(gotoWhere) {
-		var mainUrl = $.getRootPath() + gotoWhere+"/main .mainContent";
-		$("#mainView").load(mainUrl);
-	},
-	
-	load2:function(gototab){
-		var mainUrl = $.getRootPath() + "/materials"+"/main .mainContent";
-		$("#mainView").load(mainUrl);
-	},
+jQuery
+		.extend({
+			loadFunction : function(gotoWhere) {
+				var mainUrl = $.getRootPath() + gotoWhere
+						+ "/main .mainContent";
+				$("#mainView").load(mainUrl);
+			},
 
-	getGreetingTime:function () {
-		var hour = new Date().getHours();
-		var greeting = "";
-		if (hour <= 6) {
-			greeting = "现在是凌晨，注意休息。";
-		} else if (hour <= 9) {
-			greeting = "早上好！";
-		} else if (hour <= 12) {
-			greeting = "上午好！";
-		} else if (hour <= 18) {
-			greeting = "下午好！";
-		} else if (hour <= 24) {
-			greeting = "晚上好！";
-		}
-		return greeting;
-	},
-	
-	getRootPath:function() {
-        var curWwwPath = window.document.location.href;
-        var pathName = window.document.location.pathname;
-        var pos = curWwwPath.indexOf(pathName);
-        var localhostPaht = curWwwPath.substring(0, pos);
-        var projectName = pathName.substring(0, pathName.substr(1).lastIndexOf('/') + 1);
-        return (localhostPaht + projectName);
-    },
-    
-    gotoTAB:function(name) {
-    	$("#tabs a").click(function (e) {  
-            $(this).tab('show'); 
-			
-			var current = this.hash;
-			var pos = current.indexOf("Page");
-			var flag = current.substring(1,pos);
-			current = "#add"+ flag.replace(/(\w)/,function(v){return v.toUpperCase()});
-			$("#_add").attr("href",current);
+			getGreetingTime : function() {
+				var hour = new Date().getHours();
+				var greeting = "";
+				if (hour <= 6) {
+					greeting = "现在是凌晨，注意休息。";
+				} else if (hour <= 9) {
+					greeting = "早上好！";
+				} else if (hour <= 12) {
+					greeting = "上午好！";
+				} else if (hour <= 18) {
+					greeting = "下午好！";
+				} else if (hour <= 24) {
+					greeting = "晚上好！";
+				}
+				return greeting;
+			},
 
-			name = $("#_add").text().substr(0,2) + name;
-			$("#_add").text(name);
-			
-			var isLoad = $(flag +"Flag").val();
-			if(isLoad == 0){
-				//alert("Loading data");
-				$("#"+flag +"Flag").val("1")
-				$.loadFunction("/"+flag+"s");
+			getRootPath : function() {
+				var curWwwPath = window.document.location.href;
+				var pathName = window.document.location.pathname;
+				var pos = curWwwPath.indexOf(pathName);
+				var localhostPaht = curWwwPath.substring(0, pos);
+				var projectName = pathName.substring(0, pathName.substr(1)
+						.lastIndexOf('/') + 1);
+				return (localhostPaht + projectName);
+			},
+
+			gotoTAB : function(name) {
+				$("#tabs a").click(function(e) {
+					$(this).tab('show');
+
+					var current = this.hash;
+					var pos = current.indexOf("Page");
+					var flag = current.substring(1, pos);
+					current = "#add" + flag.replace(/(\w)/, function(v) {
+						return v.toUpperCase();
+					});
+					$("#_add").attr("href", current);
+
+					name = $("#_add").text().substr(0, 2) + name;
+					$("#_add").text(name);
+
+					var isLoad = $(flag + "Flag").val();
+					if (isLoad == 0) {
+						// alert("Loading data");
+						$("#" + flag + "Flag").val("1");
+						$.loadFunction("/" + flag + "s");
+					} else {
+						// alert("don't Load data");
+					}
+				});
+			},
+
+			dataSave : function(module) {
+				var form=$('#'+module+'Form');
+				if(!form.valid()){
+	                return false;
+	            }
+
+				var reqUrl = "/"+module+"s/create";
+				var errorTip = $("#errorTip");
+				var successTip=$("#successTip");
+				var modal = $("#"+module+"Modal");
+				
+
+				var content = form.serialize();
+				$.sendAjaxReq("POST", reqUrl, content, function(data,
+						textStatus) {
+					successTip.show();
+					// $("body").hideLoading();
+					modal.modal('hide');
+				}, function() {
+					errorTip.text('添加失败哦');
+					errorTip.show();
+					//modal.modal('hide');
+				});
+
+			},
+
+			sendAjaxReq : function(requestType, restUrl, content, tCallBack,
+					fCallBack) {
+				$.ajax({
+					type : requestType,
+					url : restUrl,
+					data : content,
+					success : function(data, textStatus) {
+						if (tCallBack) {
+							tCallBack(data, textStatus);
+						}
+					},
+					error : function(xmlhttp, textStatus, errorThrown) {
+						if (fCallBack) {
+							fCallBack(xmlhttp, textStatus, errorThrown);
+						}
+					}
+				});
 			}
-			else {
-				//alert("don't Load data");
-			}
-        }); 
-    }
-    
-});
 
+		});
